68ef709da26a8d5f90e726fbb8e4a9b15e2ebc19
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2015-03-26 16:27:37 +0300

    testing req variables for payload

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+    , router = express.Router()
+    , nodemailer = require('nodemailer')
+    , sgTransport = require('nodemailer-sendgrid-transport');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.render('index', {
         title: 'Express'
     });
+});
+
+router.post('/payload', function (req, res) {
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.end('success');
 });
 
 module.exports = router;
