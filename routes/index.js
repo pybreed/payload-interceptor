cc1d619649d37f6a31b27b540425463d1c3aefb2
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2015-03-26 16:53:21 +0300

    added sending email functionality

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express')
     , router = express.Router()
     , nodemailer = require('nodemailer')
-    , sgTransport = require('nodemailer-sendgrid-transport');
+    , sgTransport = require('nodemailer-sendgrid-transport'),
+    options = {
+        auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+        }
+    };
+
 
 router.get('/', function (req, res) {
     res.render('index', {
@@ -10,10 +17,20 @@ router.get('/', function (req, res) {
 });
 
 router.post('/payload', function (req, res) {
-    console.log(req.body);
-    console.log(req.params);
-    console.log(req.query);
-    res.end('success');
+    var mailer = nodemailer.createTransport(sgTransport(options))
+        , email = {
+            to: process.env.EMAIL_TO,
+            from: 'noreply@github-payload-interceptor.herokuapp.com',
+            subject: 'Payload sample',
+            text: JSON.stringify(req.body)
+        };
+
+    mailer.sendMail(email, function (err, res) {
+        if (err) { 
+            console.log(err) 
+        }
+        res.end('success');
+    })
 });
 
 module.exports = router;
